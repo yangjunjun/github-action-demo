94c02f3779dfce6a945b457ea056f0dd91d0d3af
yangjunjun <ggiiss@qq.com>
2019-10-25 07:49:09 +0000

    Deploying to gh-pages from master 5d242b36b2e5239e8745db28eabfd5ee91c43e0d

---
+++ precache-manifest.772dbbb3e22d3bc4687616b3a26cbde7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b660608bcc49b237064336f7827f359",
+    "url": "/github-action-demo/index.html"
+  },
+  {
+    "revision": "7f9a5689bdb748039fb6",
+    "url": "/github-action-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df303e49e47ecaadb034",
+    "url": "/github-action-demo/static/js/2.62acaf9c.chunk.js"
+  },
+  {
+    "revision": "7f9a5689bdb748039fb6",
+    "url": "/github-action-demo/static/js/main.cdfc508a.chunk.js"
+  },
+  {
+    "revision": "39955faecf6632653218",
+    "url": "/github-action-demo/static/js/runtime-main.4d68c18b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-action-demo/static/media/logo.25bf045c.svg"
+  }
+]);
